3675a4052411ecb68fc60b7ba3c79c731f2f8933
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2017-10-01 16:45:43 -0300

    Work on shpcb

--- pcbcmds.c
+++ pcbcmds.c
@@ -5,12 +5,25 @@
 #include <string.h>
 #include <unistd.h>
 
+#include "libs/intern.h"
+
 #include "osstate.h"
 #include "command.h"
 #include "commands.h"
 
 #include "pcbcmds.h"
 #include "pcb.h"
+
+void printpcb(struct osstate *ostate, struct pcb *pPCB) {
+	/* The name of the PCB. */
+	char *pszPCBName;
+	/* The class of the PCB. */
+	char *pszPCBClass;
+
+	fprintf(ostate->output, "PCB ID: %d\n", pPCB->id);
+	fprintf(ostate->output, "PCB Name: %s\n", lookupstring(ostate->pPCBstat->ptPCBNames, pPCB->kName));
+	fprintf(ostate->output, "PCB Class: 
+}
 
 HANDLECOM(mkpcb) { 
 	
@@ -120,6 +133,37 @@ HANDLECOM(sppcb) {
 }
 
 HANDLECOM(shpcb) {
+	/* Mode options. */
+	enum showopt { 
+		/* Show a specific PCB. */
+		SHOW_PCB,
+		/* Show a PCB queue. */
+		SHOW_QUEUE
+	};
+	/* Queue options. */
+	enum queueopt {
+		/* Show the ready queues. */
+		QU_READY,
+		/* Show the blocked queues. */
+		QU_BLOCKED,
+		/* Show all the queues. */
+		QU_ALL
+	};
+	/* Process ID options. */
+	enum pidopt {
+		/* Locate a PCB by name. */
+		PID_NAME,
+		/* Locate a PCB by number. */
+		PID_NUM
+	};
+
+	/* Chosen mode. */
+	enum showopt  mode;
+	/* Chosen queue. */
+	enum queueopt queue;
+	/* Chosen ID method. */
+	enum pidopt   idtype;
+
 	/* Reinit getopt. */
 	optind = 1;
 
@@ -129,6 +173,12 @@ HANDLECOM(shpcb) {
 		enum shpcbopt {
 			/* Help option. */
 			SH_HELP = 0,
+
+			/* Mode options. */
+			SH_MODE,       /* Specify the command mode. */
+			SH_QUEUE,      /* Specify the queue to show. */
+			SH_PID,        /* Specify the way to locate a process. */
+
 		};
 
 		/* The current option, and the current long option. */
@@ -138,10 +188,19 @@ HANDLECOM(shpcb) {
 		static const char *usage = "Usage: shpcb [-h] [--help]\n";
 
 		/* The long options we take. */
+		/* 
+		 * @TODO Use the 4th argument to indicate the corresponding
+		 * short option.
+		 */
 		static struct option opts[] = {
 			/* Misc. options. */
 			{"help", no_argument, 0, 0},
 		
+			/* Mode options. */
+			{"mode",  required_argument, 0, 0},
+			{"queue", required_argument, 0, 0},
+			{"proc",  required_argument, 0, 0},
+
 			/* Terminating argument. */
 			{0, 0, 0, 0}
 		};
@@ -152,6 +211,7 @@ HANDLECOM(shpcb) {
 		if(opt == -1) break;
 
 		/* Handle options. */
+		/* @TODO add short options. */
 		switch(opt) {
 		case 0:
 			/* Handle long options. */
@@ -159,6 +219,37 @@ HANDLECOM(shpcb) {
 			case SH_HELP:
 				fprintf(ostate->output, "%s\n", usage);
 				return 0;
+			case SH_MODE:
+				if(strcmp(optarg, "pcb")) {
+					mode = SHOW_PCB;
+				} else if(strcmp(optarg, "queue")) {
+					mode = SHOW_QUEUE;
+				} else {
+					fprintf(ostate->output, "ERROR: Invalid mode '%s'. Valid modes are 'pcb' and 'queue'\n", optarg);
+					return 1;
+				}
+			case SH_QUEUE:
+				if(strcmp(optarg, "ready")) {
+					queue = QU_READY;
+				} else if(strcmp(optarg, "blocked")) {
+					queue = QU_BLOCKED;
+				} else if(strcmp(optarg, "all")) {
+					queue = QU_ALL;
+				} else {
+					fprintf(ostate->output, "ERROR: Invalid queue '%s'. Valid queues are 'ready', 'blocked' and 'all'\n", optarg);
+					return 1;
+				}
+				break;
+			case SH_PROC:
+				if(strcmp(optarg, "name")) {
+					idtype = PID_NAME;
+				} else if(strcmp(optarg, "num")) {
+					idtype = PID_NUM;
+				} else {
+					fprintf(ostate->output, "ERROR: Invalid process ID type '%s'. Valid ID types are 'name' and 'num'\n", optarg);
+					return 1;
+				}
+				break;
 			default:
 				fprintf(ostate->output, "\tERROR: Invalid command-line argument\n");
 				fprintf(ostate->output, "%s\n", usage);
@@ -174,5 +265,60 @@ HANDLECOM(shpcb) {
 		}
 	}
 
+	switch(mode) {
+	case SHOW_PCB:
+		/* The PCB to show. */
+		struct pcb *pPCB;
+
+		switch(idtype) {
+		case PID_NAME:
+			if(optind < argc) {
+				char *pszPCBName = argv[optind];
+				pPCB = findpcbname(ostate->pPCBstat, pszPCBName);
+				if(pPCB == NULL) {
+					fprintf(ostate->output, "ERROR: No PCB with name '%s'\n", pszPCBName);
+					return 1;
+				}
+			} else {
+				fprintf(ostate->output, "ERROR: Must specify PCB name as argument.\n");
+				return 1;
+			}
+			break;
+		case PID_NUM:
+			if(optind < argc) {
+				int pcbid = atoi(argv[optind]);
+				pPCB = findpcbnu,(ostate->pPCBstat, pcbid);
+				if(pPCB == NULL) {
+					fprintf(ostate->output, "ERROR: No PCB with ID '%d'\n", pcbid);
+					return 1;
+				}
+			} else {
+				fprintf(ostate->output, "ERROR: Must specify PCB name as argument.\n");
+				return 1;
+			}
+			break;
+		default:
+			/* Shouldn't happen. */
+			assert(0);
+		}
+		printpcb(ostate, pPCB);
+		break;
+	case SHOW_QUEUE:
+		switch(queue) {
+		case QU_READY:
+			break;
+		case QU_BLOCKED:
+			break;
+		case QU_ALL:
+			break;
+		default:
+			/* Shouldn't happen. */
+			assert(0);
+		}
+		break;
+	default:
+		/* Shouldn't happen. */
+		assert(0);
+	}
 	return 0;
 }
