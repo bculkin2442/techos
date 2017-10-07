be9e6951412a55c4119fb618e893b0374dca4595
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2017-10-07 19:32:15 -0300

    Fix single-word arguments

--- libs/argparser.c
+++ libs/argparser.c
@@ -5,30 +5,32 @@
 #include "argparser.h"
 
 /* Parse a string from a set of arguments. */
-static char *parsestr(char *lead, char delim, char **args) {
+static char *parsestr(char *pszLead, char delim, char **pSaved) {
 	/* The rest of the string from the arguments. */
 	char *rest;
 	/* The string we're returning. */
-	char *strang;
+	char *pszStrang;
 
 	/* The rest of the strings contents. */
-	rest = (char *)strtok_r(NULL, &delim, args);
+	rest = (char *)strtok_r(NULL, &delim, pSaved);
 
 	/* Print the string, and bail if that fails. */
-	if(asprintf(&strang, "%s %s", lead, rest) == -1) assert(0);
+	if(asprintf(&pszStrang, "%s %s", pszLead, rest) == -1) assert(0);
 
-	return strang;
+	return pszStrang;
 }
 
 /* Parse a set of CLI arguments. */
-struct cliargs parseargs(char *strang) {
+struct cliargs parseargs(char *pszStrang) {
 	/* The return value. */
 	struct cliargs args;
 
 	/* The pointer for parsing. */
 	char *pSave;
 	/* The currently parsed token. */
-	char *token;
+	char *pszToken;
+	/* Length of current token. */
+	int tokenlen;
 
 	/* Initialize argument count. */
 	args.argc = 0;
@@ -38,20 +40,41 @@ struct cliargs parseargs(char *strang) {
 	assert(args.argv != NULL);
 
 	/* Get the initial tokens. */
-	token     = (char *)strtok_r(strang, " ", &pSave);
+	pszToken = (char *)strtok_r(pszStrang, " ", &pSave);
 
-	while(token != NULL && args.argc < MAX_CLI_ARGS) {
-		if(token[0] == '\'' || token[0] == '\"') {
-			/* @TODO Handle single word strings. */
-			/* Parse a string. */
-			args.argv[args.argc] = parsestr(token + 1, token[0], &pSave);
+	while(pszToken != NULL && args.argc < MAX_CLI_ARGS) {
+		tokenlen = strlen(pszToken);
+
+		/* The possible quote. */
+		char posQuote;
+		/* The slot the arg goes into. */
+		char **ppszArg;
+
+		posQuote = pszToken[0];
+		ppszArg  = &(args.argv[args.argc]);
+
+		if(posQuote == '\'' || posQuote == '\"') {
+			if(pszToken[tokenlen - 1] == posQuote) {
+				/* Length of single-string. */
+				int stranglen;
+
+				/* Single word string, no parsing needed */
+				*ppszArg = (char *)strdup(pszToken + 1);
+				stranglen = strlen(*ppszArg);
+
+				/* Pull off the ending quote. */
+				ppszArg[0][stranglen - 1] = '\0';
+			} else {
+				/* Parse a string. */
+				*ppszArg = parsestr(pszToken + 1, posQuote, &pSave);
+			}
 		} else {
 			/* No string, just add the argument. */
-			args.argv[args.argc] = (char *)strdup(token);
+			*ppszArg = (char *)strdup(pszToken);
 		}
 
 		/* Get the next token. */
-		token      = (char *)strtok_r(NULL, " ", &pSave);
+		pszToken   = (char *)strtok_r(NULL, " ", &pSave);
 		args.argc += 1;
 	}
 
