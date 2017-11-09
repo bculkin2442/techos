eaced96c5fbe7fae0f88de1c1789cca02c39260c
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2017-11-09 18:57:00 -0400

    Fix working directory establishment

--- osstate.c
+++ osstate.c
@@ -57,7 +57,11 @@ struct osstate *makeosstate() {
 	ostate->pComstate = makecommandstate();
 
 	/* Setup working directory. */
-	ostate->fWorkingDir = open("tree", 0);
+	ostate->fWorkingDir = open("tree", O_RDONLY);
+	if(ostate->fWorkingDir == -1) {
+		fprintf(ostate->output, "FATAL ERROR: Could not set working directory\n");
+		exit(1);
+	}
 
 	return ostate;
 }
