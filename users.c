3d5f2219e720b493a9cb0a0558dbdaaf0b36ded6
Benjamin J. Culkin <bjculkin@mix.wvu.edu>
2017-11-29 18:56:16 -0400

    Load/Store user files

--- users.c
+++ users.c
@@ -60,3 +60,95 @@ void udbinsert(struct userdb *pdDB, enum utype type, char *pszUsername, char *ps
 void udbremove(struct userdb *pdDB, char *pszUsername) {
 	deleteinternlist(pdDB->plUsers, pszUsername);
 }
+
+/* Load user database from a file. */
+void ramfileintodb(struct userdb *pdDB, char *pszFilename) {
+	FILE *pfFile;
+
+	char *pszLine;
+	size_t  llen;
+	size_t lread;
+
+	llen = 0;
+
+	pfFile = fopen(pszFilename, "r");
+	if(pfFile == NULL) {
+		return;
+	}
+
+	while((lread = getline(&pszLine, &llen, pfFile)) != -1) {
+		if(feof(pfFile)) break;
+		
+		char *pszName, *pszPass, *pszType, *pSave;
+		enum utype type;
+
+		/* Read username. */
+		pszName = (char *)strtok_r(pszName, ":", &pSave);
+		/* Skip improperly formatted records. */
+		if(pszName == NULL) continue;
+
+		/* Read password. */
+		pszPass = (char *)strtok_r(NULL, ":", &pSave);
+		/* Skip improperly formatted records. */
+		if(pszPass == NULL) continue;
+
+		/* Read user type. */
+		pszType = (char *)strtok_r(NULL, ":", &pSave);
+		/* Skip improperly formatted records. */
+		if(pszType == NULL) continue;
+
+		/* Parse user type. */
+		if(strncmp("rim", pszType, 3) == 0) {
+			type = UTY_BASIC;
+		} else if(strncmp("spoke", pszType, 5) == 0) {
+			type = UTY_ADMIN;
+		} else if(strncmp("hub", pszType, 3) == 0) {
+			type = UTY_ROOT;
+		} else {
+			/* Invalid user type. */
+			continue;
+		}
+
+		udbinsert(pdDB, type, pszName, pszPass);
+	}
+
+	fclose(pfFile);
+}
+
+void offloaduser(char *pszUsername, void *pvUser, void *pvData) {
+	struct user *puUser;
+
+	FILE *pfOutput;
+
+	char *pszKind;
+
+	puUser   = (struct user *)pvUser;
+	pfOutput = (FILE *)pvData;
+
+	switch(puUser->type) {
+	case UTY_BASIC:
+		pszKind = "rim";
+		break;
+	case UTY_ADMIN:
+		pszKind = "spoke";
+		break;
+	case UTY_ROOT:
+		pszKind = "hub";
+		break;
+	default:
+		/* Shouldn't happen. */
+		assert(0);
+	}
+
+	fprintf(pfOutput, "%s:%s:%s\n", puUser->pszName, puUser->pszPass, pszKind);
+}
+
+/* Save user database to a file. */
+void ramdbintofile(struct userdb *pdDB, char *pszFilename) {
+	FILE *pfFile = fopen(pszFilename, "w");
+	if(pfFile == NULL) return;
+
+	foreachinternlist(pdDB->plUsers, &offloaduser, pfFile);
+
+	fclose(pfFile);
+}
